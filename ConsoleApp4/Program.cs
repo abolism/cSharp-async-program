6c79fd57f4754961029572e047d5f57b304217c9
Academy 10 <Academy 10@ACADEMY-010>
2022-03-09 14:43:27 +0330

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    public class Program
+    {
+
+        public static async Task Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            var music = choseMusic();
+            //Console.WriteLine("music playing");
+
+            var musicTask = playMusic(music);
+            var clothesTask = getClothed(3);
+
+            var musicPlaying = await musicTask;
+            Console.WriteLine("music finished");
+
+            var gettingClothed = await clothesTask;
+            Console.WriteLine("got clothed");
+
+            var doorTask = openningTheDoor();
+            var carTask = startingCar();
+            var openingDoor = await doorTask;
+            Console.WriteLine("door Opened");
+
+            
+            var startingTheCar = await carTask;
+            Console.WriteLine("car started");
+        }
+        static object choseMusic()
+        {
+            Console.WriteLine("chosed music");
+            return new object();
+        }
+        static async Task<Object> playMusic(object obj)
+        {
+            Console.WriteLine("playing music");
+            await Task.Delay(3000);
+            return new object();
+        }
+        static async Task<Object> getClothed(int pieces)
+        {
+            Console.WriteLine("getting clothed");
+            await Task.Delay(4000 * pieces);
+            return new object();
+        }
+        static async Task<Object> openningTheDoor()
+        {
+            Console.WriteLine("opening the door");
+            await Task.Delay(1500);
+            return new object();
+        }
+        static async Task<Object> startingCar()
+        {
+            Console.WriteLine("starting car");
+            await Task.Delay(5000);
+            return new object();
+        }
+    }
+    //public class Tasks
+    //{
+
+    //}
+}
